c2f0ffd2d74054b13b4cefb427cf28cda91e61f5
Marielly Silva <mariellymartinsdasilva9@gmail.com>
2025-07-02 17:31:16 -0300

    User entity

---
+++ src/main/java/com/example/systemLogin/DataConfiguration.java
@@ -0,0 +1,32 @@
+package com.example.systemLogin;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfiguration {
+	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/systemLogin?useTimezone=true&serverTimezone=UTC");
+		dataSource.setUsername("developer");
+		dataSource.setPassword("1234567");
+		return dataSource;
+	}
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MariaDBDialect");
+		return adapter;
+	}
+
+}
