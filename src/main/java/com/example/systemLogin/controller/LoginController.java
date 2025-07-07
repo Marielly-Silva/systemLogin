92ca21190508a459214aee609d0acf1154acb1ce
Marielly Silva <mariellymartinsdasilva9@gmail.com>
2025-07-07 17:58:45 -0300

    Encrypted password

--- src/main/java/com/example/systemLogin/controller/LoginController.java
+++ src/main/java/com/example/systemLogin/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.example.systemLogin.controller;
 import java.io.UnsupportedEncodingException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -24,6 +25,8 @@ public class LoginController {
 	
 	@Autowired
 	private UserRepository ur;
+	@Autowired
+    private PasswordEncoder passwordEncoder;
 	
 	@GetMapping("/login")
 	public String login() {
@@ -38,12 +41,18 @@ public class LoginController {
 	
 	@PostMapping("/logar")
 	public String logged(User user, Model model, HttpServletResponse response) throws UnsupportedEncodingException {
-	    User loggedUser = this.ur.login(user.getEmail(), user.getPassword());
-	    if (loggedUser != null) {
+		 User loggedUser = ur.findByEmail(user.getEmail()); 
+		    if (loggedUser  != null) {
+		        String passwordEntered = user.getPassword();
+		        String passwordEncrypted  = loggedUser.getPassword();
+
+		
+		        if (passwordEncoder.matches(passwordEntered, passwordEncrypted)) {
 	    	CookieService.setCookie(response, "userId",String.valueOf(loggedUser.getId()), 100000);
 	    	CookieService.setCookie(response, "nameUser",String.valueOf(loggedUser.getName()), 100000);
 	        return "redirect:/";
-	    }
+		        }
+		    }
 	    model.addAttribute("erro", "Email ou senha inválidos!");
 	    return "login";
 	}
@@ -60,7 +69,13 @@ public class LoginController {
 	}
 	
 	@RequestMapping(value="/register", method = RequestMethod.POST)
-	public String userRegistration(@Valid User user, BindingResult result) {
+	public String userRegistration(@Valid User user, BindingResult result, Model model) {
+		if (ur.findByEmail(user.getEmail()) != null) {
+	        model.addAttribute("erro", "Este e-mail já está cadastrado!");
+	        return "cadastro";
+	    }
+		String encoder = this.passwordEncoder.encode(user.getPassword());
+		user.setPassword(encoder);
 		if(result.hasErrors()) {
 			return "redirect:/register";
 		}
--- src/main/java/com/example/systemLogin/repository/UserRepository.java
+++ src/main/java/com/example/systemLogin/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.example.systemLogin.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.systemLogin.model.User;
@@ -8,7 +7,6 @@ import com.example.systemLogin.model.User;
 public interface UserRepository extends CrudRepository<User, Long> {
 
 	User findById(long id);
-	
-	@Query(value = "SELECT * FROM systemlogin.user WHERE email = :email AND password = :password", nativeQuery = true)
-	public User login(String email, String password);
+
+	User findByEmail(String email);
 }
